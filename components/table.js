836d1f8ef1bf06034c1c43c3db7c9c8c636c6afa
Stephen DeLorme <stephen@d.elor.me>
2022-03-04 10:48:19 -0500

    Added bitcoin icons to table

--- components/table.js
+++ components/table.js
@@ -1,7 +1,10 @@
+import { CrossIcon } from '@bitcoin-design/bitcoin-icons-react/filled';
+import { CheckIcon } from '@bitcoin-design/bitcoin-icons-react/filled';
+
 function Item({ status, text, title, children }) {
   return (
     <div className="flex space-x-4">
-      <div>{status ? "x" : "0"}</div>
+      <div>{status ? <CheckIcon className="w-8 h-8 text-bpr-blue" /> : <CrossIcon className="w-8 h-8 text-bpr-pink" />}</div>
       <div>{title} </div>
       <div>{text}</div>
     </div>
