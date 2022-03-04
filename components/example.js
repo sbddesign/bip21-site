85809ff3f9dcac270dcb8e4c02e948fd02adbb45
Stephen DeLorme <stephen@d.elor.me>
2022-03-04 08:58:32 -0500

    Styling updates for Problems, Solutions, Examples, etc.

--- components/example.js
+++ components/example.js
@@ -1,8 +1,10 @@
-export default function Example({ title, caption, raw }) {
+export default function Example({ title, caption, raw, src }) {
   return (
     <div className="flex flex-col items-center space-y-4 px-4">
       <h4>{title}</h4>
-      <div className="w-[340px] h-[340px] bg-white drop-shadow-md">qr!</div>
+      <div className="bg-white drop-shadow-lg">
+          <img src={src} alt={title} className="w-full" />
+      </div>
       <p>{caption}</p>
       <h4>Raw Data</h4>
       <pre className="bg-bpr-code-gray break-all p-4 rounded whitespace-pre-wrap">
